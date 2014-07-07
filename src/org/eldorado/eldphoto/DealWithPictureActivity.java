38ae1359b88822ecf1f7ef24e17e59858c9096b5
p-hack <phack@motorola.com>
2014-07-07 16:20:26 -0300

    Addition of commentaries and little changes.

--- src/org/eldorado/eldphoto/DealWithPictureActivity.java
+++ src/org/eldorado/eldphoto/DealWithPictureActivity.java
@@ -35,14 +35,15 @@ import android.widget.ViewFlipper;
 public class DealWithPictureActivity extends Activity {
 
 	public static final String PICTURE = "org.eldorado.eldphoto.PICTURE";
-	private byte[] picture;
-	private Bitmap image;
-	private static Bitmap currentImage = null;
-	private ViewFlipper viewFlipper;
-	private float lastX;
-	private static ArrayList<Bitmap> filterImages = new ArrayList();
+	private byte[] picture; //the picture taken as a byte array
+	private Bitmap image; //the picture taken as a bitmap
+	private static Bitmap currentImage = null; //the image displayed (can be the original or a effect-applied one)
+	private ViewFlipper viewFlipper; //the view which contains the graphical components to show the effects/filters options
+	private float lastX; //the last X position of the screen touch event
+	private static ArrayList<Bitmap> filterImages = new ArrayList(); //the list with the effects/filters thumbnail images
 	private Context context = this;
-	private EffectsFactory effectsFactory;
+	private EffectsFactory effectsFactory; //the effects factory object
+	private static boolean isThereEffectApplied = false; //whether there is an effect/filter applied to the original image being displayed
 	
 	public DealWithPictureActivity() {
 		
@@ -62,24 +63,35 @@ public class DealWithPictureActivity extends Activity {
 		options.inPurgeable = true; //allows the system to reclaim memory
 		options.inInputShareable = true; //keeps a shallow reference to the data
 		
+		//converts the byte array into bitmap
 		image = BitmapFactory.decodeByteArray(picture, 0, picture.length, options);
+
+		//if this is the first time the activity is created for that picture, ...
 		if(currentImage == null)
+			//saves the image as the current image
 			currentImage = image.copy(image.getConfig(), true);
-		else{
+		
+		//otherwise, if some effect/filter was applied, ...
+		else if(isThereEffectApplied){
 			
+			//makes the undo button visible
 			Button undoButton = (Button) findViewById(R.id.undoButton);
 			undoButton.setVisibility(View.VISIBLE);
 		}
 		
+		//displays the current image
 		ImageView imageView = (ImageView) findViewById(R.id.imageView1);
 		imageView.setAdjustViewBounds(true);
 		imageView.setImageBitmap(currentImage);
 		
+		//sets the background of the flipper (where will be displayed the effects thumbnails) to be transparent
 		viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper1);
 		viewFlipper.setBackgroundColor(Color.argb(90, 255, 255, 255));
 		
+		//sends the context information to effectsFactory class
 		effectsFactory.setContext(this);
 		
+		//displays the filters/effects options, when available
 		showFilters();
 	}
 	
@@ -138,174 +150,241 @@ public class DealWithPictureActivity extends Activity {
         return false;
 	}
 	
-	/** This method is to be written.
-	 * It should check if there is available filters/effects and display them.
-	 * 
+	/** Shows the effects/filters implementation available.
+	 * It calls the implementation and apply them on thumbnails.
+	 * If this is the first time it is being called with the same picture,
+	 * the thumbnails are saved into a list so the next times, they will only
+	 * be displayed.
 	 */
 	public void showFilters(){
 		
-		//for each effects available, ...
 		try{
-			
+			//checks for the available filters/effects (this must be called before 'getNumberOfEffectsAvailable()' method)
 			EffectsFactory.getAvailableEffects();
 		}
 		catch(Exception ex){
 			
+			//if there was a problem, display its message
 			Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+			//prints its stack trace for debugging
 			ex.printStackTrace();
+			//gets out of this method
 			return;
 		}
 		
+		//if this is the first time we show the filters thumbnails, they shall be processed
 		if(filterImages.size() == 0){
-			
+			//for each of the available effects/filters...
 			for(int id = 0; id < EffectsFactory.getNumberOfEffectsAvailable(); id++){
 
 				try{
+					//creates the graphical components to display the thumbnails 
+					//the layout
 					LinearLayout layout = new LinearLayout(this);
 					layout.setOrientation(LinearLayout.VERTICAL);
 					layout.setGravity(Gravity.CENTER);
-
+					
+					//the text view with the filter/effect's name
 					TextView textView = new TextView(this);
 					textView.setText(effectsFactory.getEffect(id).getName());
 					textView.setGravity(Gravity.CENTER);
-
+					
+					//the image view with the thumbnail with the filter/effect applied
 					ImageView filterView = new ImageView(this);
 					filterView.setAdjustViewBounds(true);
 					filterView.setMaxHeight(200);
 					filterView.setMaxWidth(200);
 
+					//gets the dimensions of the original image
 					int width = image.getWidth();
 					int height = image.getHeight();
 
+					//computes the dimensions of the thumbnail according to the dimensions of the original image
+					//such that the greatest dimension should equal 200 px
+					//if the width is bigger than height, then width should be 200px
 					if(width > height){
-
+						
+						//sets the height proportionally
 						height = Math.round(height * 200.0f/width);
 						width = 200;
 					}
+					//else, the height should be 200px
 					else{
+						//sets the width proportionally
 						width = Math.round(width * 200.0f/height);
 						height = 200;
 					}
-
+					
+					//adds into the list 'filterImages' the thumbnail with the filter/effect applied and with he computed dimensions
 					filterImages.add(effectsFactory.getEffect(id).applyEffect(Bitmap.createScaledBitmap(image, width, height, false)));
 
+					//sets the image view's image bitmap to the thumbnail
 					filterView.setImageBitmap(filterImages.get(id));
 
-					layout.addView(textView);
-					layout.addView(filterView);
-					viewFlipper.addView(layout);
+					layout.addView(textView); //puts the text view inside the layout
+					layout.addView(filterView); //puts the image view inside the layout
+					viewFlipper.addView(layout); //puts the layout inside the flipper view
 
+					//sets the method that will be called when the image view with the thumbnail is clicked
 					layout.setOnClickListener(new OnClickListener() {
 
 						@Override
 						public void onClick(View view) {
 
 							try{
+								//gets the image view with the current image
 								ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+
+								//gets the name of the clicked filter/effect thumbnail
 								LinearLayout layout = (LinearLayout) view;
 								TextView text = (TextView) layout.getChildAt(0);
 								String str = (String) text.getText();
 								int id = 0;
 
+								//gets the id corresponding to such filter/effect
 								for(int i = 0; i < EffectsFactory.getNumberOfEffectsAvailable(); i++)
 									if(str.compareTo(effectsFactory.getEffect(i).getName()) == 0){
 										id = i;
 										break;
 									}
 
+								//applies the filter/effect to the original image and makes it the current one
 								currentImage = effectsFactory.getEffect(id).applyEffect(
 										Bitmap.createScaledBitmap(image, imageView.getWidth(), imageView.getHeight(), false));
 
+								//displays the new image with the filter/effect applied
 								imageView.setImageBitmap(currentImage);
 
+								//makes the undo button visible
 								Button undoButton = (Button) findViewById(R.id.undoButton);
 								undoButton.setVisibility(View.VISIBLE);
+								
+								//signalizes that an effect/filter was applied
+								isThereEffectApplied = true;
 							}
 							catch(Exception ex){
 
+								//if something goes wrong, displays the error message
 								Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
 							}
 						}
 					});
 				}
 				catch(Exception ex){
+					//if something goes wrong, displays the error message
 					Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
 				}
 			}
 		}
+		//else, if this isn't the first time we compute the thumbnails for the filters/effects for that picture...
 		else{
 			
+			//for each of the available filters/effects, ...
 			for(int id = 0; id < EffectsFactory.getNumberOfEffectsAvailable(); id++){
 
 				try{
+					//creates the graphical components to display the thumbnails 
+					//the layout
 					LinearLayout layout = new LinearLayout(this);
 					layout.setOrientation(LinearLayout.VERTICAL);
 					layout.setGravity(Gravity.CENTER);
 
+					//the text view with the filter/effect's name
 					TextView textView = new TextView(this);
 					textView.setText(effectsFactory.getEffect(id).getName());
 					textView.setGravity(Gravity.CENTER);
 
+					//the image view with the thumbnail with the filter/effect applied
 					ImageView filterView = new ImageView(this);
 					filterView.setAdjustViewBounds(true);
 					filterView.setMaxHeight(200);
 					filterView.setMaxWidth(200);
 
+					//sets the image view's image bitmap to the thumbnail
 					filterView.setImageBitmap(filterImages.get(id));
 
-					layout.addView(textView);
-					layout.addView(filterView);
-					viewFlipper.addView(layout);
+					layout.addView(textView); //puts the text view inside the layout
+					layout.addView(filterView); //puts the image view inside the layout
+					viewFlipper.addView(layout); //puts the layout inside the flipper view
 
+					//sets the method that will be called when the image view with the thumbnail is clicked
 					layout.setOnClickListener(new OnClickListener() {
 
 						@Override
 						public void onClick(View view) {
 
 							try{
+								//gets the image view with the current image
 								ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+
+								//gets the name of the clicked filter/effect thumbnail
 								LinearLayout layout = (LinearLayout) view;
 								TextView text = (TextView) layout.getChildAt(0);
 								String str = (String) text.getText();
 								int id = 0;
 
+								//gets the id corresponding to such filter/effect
 								for(int i = 0; i < EffectsFactory.getNumberOfEffectsAvailable(); i++)
 									if(str.compareTo(effectsFactory.getEffect(i).getName()) == 0){
 										id = i;
 										break;
 									}
 
+								//applies the filter/effect to the original image and makes it the current one
 								currentImage = effectsFactory.getEffect(id).applyEffect(
 										Bitmap.createScaledBitmap(image, imageView.getWidth(), imageView.getHeight(), false));
 
+								//displays the new image with the filter/effect applied
 								imageView.setImageBitmap(currentImage);
 
+								//makes the undo button visible
 								Button undoButton = (Button) findViewById(R.id.undoButton);
 								undoButton.setVisibility(View.VISIBLE);
+								
+								//signalizes that an effect/filter was applied
+								isThereEffectApplied = true;
 							}
 							catch(Exception ex){
+
+								//if something goes wrong, displays the error message
 								Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
 							}
 						}
 					});
 				}
 				catch(Exception ex){
+					//if something goes wrong, displays the error message
 					Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
 				}
 			}
 		}
 	}
 	
+	/** Removes the filter/effect from the preview picture.
+	 * It sets the image view's bitmap to 'image' (the original picture taken).
+	 * 
+	 * @param view
+	 */
 	public void undoFilter(View view){
 		
+		//sets the 'currentImage' to the original one ('image')
 		currentImage = image;
+		//sets the image view's image picture to the original one
 		ImageView imageView = (ImageView) findViewById(R.id.imageView1);
 		imageView.setImageBitmap(currentImage);
 		
+		//makes the undo button invisible
 		Button undoButton = (Button) findViewById(R.id.undoButton);
 		undoButton.setVisibility(View.INVISIBLE);
+		
+		//signalizes that an effect/filter wasn't applied
+		isThereEffectApplied = false;
 	}
 	
+	/** Called when cancel button is clicked.
+	 * It finalizes the activity.
+	 * @param view
+	 */
 	public void cancelPicture(View view){
 		
 		this.finish();
@@ -326,6 +405,9 @@ public class DealWithPictureActivity extends Activity {
 		filterImages.clear();
 	}
 	
+	/** Removes the current image by setting it to null.
+	 * 
+	 */
 	public static void removeCurrentImage(){
 		currentImage = null;
 	}
--- src/org/eldorado/eldphoto/EffectsFactory.java
+++ src/org/eldorado/eldphoto/EffectsFactory.java
@@ -16,78 +16,100 @@ import android.graphics.Bitmap;
 import android.os.Environment;
 import android.widget.Toast;
 
+/** This class should provide access to the filters/effects "plugins" methods.
+ * It loads the classes (Effects implementations) specified in "../DCIM/EldPhoto/Effects/classes.txt" and
+ * acts as an interface to its methods 'applyEffect' and 'getName'.
+ * 
+ * @author phack
+ *
+ */
 public class EffectsFactory {
 
-	public static final int HISTOGRAM_EQUALIZATION = 0;
-	public static final int INVERSE = 1;
-	private static final int NUMBER_OF_EFFECTS = 2;
-	private Context context;
-	private Class<?> effectClass;
-	private Object effectObj;
-	private static ArrayList<String> classes;
+	private Context context; //the application context, used to get access to the application's cache directory
+	private Class<?> effectClass; //the effect class that will be dynamically loaded
+	private Object effectObj; //the object that implements the effect class
+	private static ArrayList<String> classes; //the names of the classes to be loaded
 	
-	/** Returns an Effect object specified by the 'id'.
+	/** Sets the effect class and object so that their methods can be called next.
+	 * It returns a reference of itself, so the desired object's method can be accessed in chain.
+	 * Example:<p>
+	 *     effectsFactory.getEffect(1).getName(); <p>
+	 * This will return the name of the 2nd effect loaded, if no error occurs.
 	 * 
 	 * @param id - int - The id specifying the type of effect.
-	 * @return Effect - An object of the specified effect or null.
+	 * @return EffectFactory - a reference to itself (the caller).
+	 * @exception Exception - if the folder "../DCIM/EldPhoto/Effects/" cannot be accessed nor created; if 
+	 * a problem occurs loading or instantiating the classes.
 	 */
 	public EffectsFactory getEffect(int id)
 	throws Exception{
 		
 		try{
 			
+			//checks if the external storage is readable
 			if(isExternalStorageReadable()){
-				
+				//tries to access the folder "../DCIM/EldPhoto/Effects/", where the classes should be
 				File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "EldPhoto/Effects/");
+
+				//if this path is not a directory, ...
 				if(!folder.isDirectory())
+					//tries to create such directory
+					//if it fails, ...
 					if(!folder.mkdirs()){
-						
-						throw new Exception("Couldn't access effects folder");
+						//throws an exception
+						throw new Exception("Couldn't find nor create the needed folder!");
 					}
 				
-				//String path = folder.getAbsolutePath();
+				//if the folder was successfully accessed, ...
+				//gets the path to the APK file, containing the classes' definitions
 				String path = folder.getAbsolutePath() + "/EffectClasses.apk";
-				//File dexOpt = new File(context.getCacheDir(), "dex");
+				
+				//gets the path to a cache directory where the optimized classes version will be created by Android
 				File dexOpt = context.getCacheDir();
-				//ClassLoader EffectClassLoader = new EffectsLoader(path, dexOpt, ClassLoader.getSystemClassLoader());
+				
+				//constructs the class loader, which will load the classes from the APK file
 				ClassLoader EffectClassLoader = new DexClassLoader(path, dexOpt.getAbsolutePath(), null, ClassLoader.getSystemClassLoader());
 				
-//				switch(id){
-//				case HISTOGRAM_EQUALIZATION:
-//					effectClass = EffectClassLoader.loadClass("org.eldorado.effectclasses.HistogramEqualizationEffect");
-//					effectObj = effectClass.newInstance();
-//					//effect = EffectMap.get(clazz.getName()).newInstance();
-//					break;
-//				case INVERSE:
-//					effectClass = EffectClassLoader.loadClass("org.eldorado.effectclasses.InverseEffect");
-//					effectObj = effectClass.newInstance();
-//					//effect = EffectMap.get(clazz.getName()).newInstance();
-//					break;
-//				}
-				
+				//loads the (id + 1)th class
 				effectClass = EffectClassLoader.loadClass(classes.get(id));
+				//constructs an object of this class
 				effectObj = effectClass.newInstance();
 			}
+			//if the external storage can't be read, ...
 			else{
+				//throws an exception
 				throw new Exception("The storage isn't ok for reading");
 			}
+			//returns a reference to itself (the caller)
 			return this;
 		}
 		catch(Exception ex){
-			throw new Exception(ex.getMessage());
+			//if some error occurs, throws an exception with its message and cause
+			throw new Exception(ex.getMessage(), ex);
 		}
 	}
 	
+	/** Calls the method "applyEffect" from the object 'effectObj' and returns its result.
+	 * 
+	 * @param bitmap - The input image.
+	 * @return Bitmap - The resulting image, after the application of the filter/effect or 
+	 * null if the object wasn't correctly loaded.
+	 * @throws Exception - if the method couldn't be found; if the arguments were invalid;
+	 * if the object was invalid.
+	 */
 	public Bitmap applyEffect(Bitmap bitmap)
 	throws Exception{
 		
+		//the resulting image
 		Bitmap picture = null;
 		
+		//only proceeds if both class and object are different from null
 		if(effectClass != null && effectObj != null){
 			
 			try{
-				
+				//gets the desired method, 'applyEffect'
 				Method applyEffectMethod = effectClass.getMethod("applyEffect", new Class[]{Bitmap.class});
+				//calls the method from the object 'effectObj' with the input image as argument
 				picture = (Bitmap) applyEffectMethod.invoke(effectObj, bitmap);
 			}
 			catch(NoSuchMethodException ex){
@@ -112,19 +134,30 @@ public class EffectsFactory {
 			}
 		}
 		
+		//returns the result
 		return picture;
 	}
 	
+	/** Calls the method 'getName' from the object loaded 'effectObj' and returns it result.
+	 * 
+	 * @return String - The name of the effect/filter loaded or null, if the effect object
+	 * wasn't correctly loaded.
+	 * @throws Exception - if the method couldn't be found; if the arguments were invalid;
+	 * if the object was invalid.
+	 */
 	public String getName()
 	throws Exception{
 
+		//the resulting effect name
 		String name = null;
 
+		//only proceeds if both class and object are different from null
 		if(effectClass != null && effectObj != null){
 
 			try{
-
+				//gets the method 'getName' from the class loaded
 				Method getNameMethod = effectClass.getMethod("getName", new Class<?>[0]);
+				//calls the method from the object 'effectObj' and no argument
 				name = (String) getNameMethod.invoke(effectObj, (Object[]) null);
 			}
 			catch(NoSuchMethodException ex){
@@ -149,6 +182,7 @@ public class EffectsFactory {
 			}
 		}
 
+		//returns the result
 		return name;
 	}
 	
@@ -157,11 +191,15 @@ public class EffectsFactory {
 	 * @return int - The number of effects available.
 	 */
 	public static int getNumberOfEffectsAvailable(){
-		
+		//returns the size of the list containing the names of the classes
 		return classes.size();
 	}
 	
-	/* Checks if external storage is available to at least read */
+	/** Checks if external storage is available to, at least, read
+	 * 
+	 * @return TRUE, if the the external storage is available to read;
+	 * FALSE, otherwise.
+	 */
 	public static boolean isExternalStorageReadable() {
 	    String state = Environment.getExternalStorageState();
 	    if (Environment.MEDIA_MOUNTED.equals(state) ||
@@ -171,33 +209,57 @@ public class EffectsFactory {
 	    return false;
 	}
 	
+	/** Sets the application context, needed to get access
+	 * to the application's cache directory.
+	 * If the given context is null, nothing changes.
+	 * 
+	 * @param theContext - Context - The application context.
+	 */
 	public void setContext(Context theContext){
 		if(theContext != null)
 			context = theContext;
 	}
 	
+	/** Checks the availability of effects/filters.
+	 * It tries to access the file "../DCIM/EldPhoto/Effects/classes.txt" and read the
+	 * classes' names from it. If it succeeds, the classes' names will be stored on the
+	 * 'classes' list.
+	 * 
+	 * @throws Exception - if some error reading the file occurs.
+	 */
 	public static void getAvailableEffects()
 	throws Exception{
 
-		classes = new ArrayList();
+		//initializes the classes' names list, if it is null
+		if(classes == null)
+			classes = new ArrayList();
 
+		//if we have access to the external storage, ...
 		if(EffectsFactory.isExternalStorageReadable()){
 
+			//tries to access the folder "../DCIM/EldPhoto/Effects/"
 			File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "EldPhoto/Effects/");
+			//if the folder isn't a directory, ...
 			if(!folder.isDirectory())
+				//tries to create it
+				//if can't be created, ...
 				if(!folder.mkdirs()){
-
+					//throws an exception
 					throw new Exception("Couldn't access effects folder");
 				}
 			try{
+				//tries to access the file classes.txt from the 'folder'
 				File classesFile = new File(folder.getAbsoluteFile() + "/classes.txt");
 				BufferedReader reader = new BufferedReader(new FileReader(classesFile));
 
+				//reads its first line
 				String className = reader.readLine(); 
-
+				//keeps reading its lines until there's no more
 				while(className != null){
 
+					//for each class name, adds it into the list
 					classes.add(className);
+					//and tries to read the next
 					className = reader.readLine();
 				}
 			}
