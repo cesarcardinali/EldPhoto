bd20e3b2e70cdcb0c18de917bac3345c00fbc073
Cesar <cesarcardinali@hotmail.com>
2014-08-26 14:24:26 -0300

    App funcionando com library de efeitos linkada diretamente

---
+++ src/org/eldorado/eldphoto/support/Constant.java
@@ -0,0 +1,8 @@
+package org.eldorado.eldphoto.support;
+
+public class Constant {
+	public static String LOG_TAG = "AndroidImageFilterDemo";
+	public static String[] styles = {"GrayScale", "Relief", "Average Blur", "Oil Painting", "Neon", "Pixelate", "Old TV", 
+	                                 "Invert Color", "Block", "Aged photo", "Sharpen", "Light", "Lomo", "HDR", "Gaussian Blur",
+	                                 "Soft Glow", "Sketch", "Motion Blur", "Gotham", "Eldorado"};
+}
